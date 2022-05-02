e7eb9a096c73f37df0b9d3bef7842e4be3e51036
BENJAMIN STANO <BSTANO9@GMAIL.COM>
2022-05-02 16:05:49 -0400

    added basic tests

--- tests/testthat/test-ggedf.R
+++ tests/testthat/test-ggedf.R
@@ -1,3 +1,11 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("stat_per_sum is ggproto", {
+  expect_true(ggplot2::is.ggproto(stat_per_sum()))
+})
+
+test_that("stat_quant is ggproto", {
+  expect_true(ggplot2::is.ggproto(stat_quant()))
+})
+
+test_that("stat_cum_per_poly is ggproto", {
+  expect_true(ggplot2::is.ggproto(stat_cum_per_poly()))
 })
