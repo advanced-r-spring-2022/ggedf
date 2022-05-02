16de157b2c78a08125c5cc72090fabebd1e586bc
BENJAMIN STANO <BSTANO9@GMAIL.COM>
2022-05-02 19:24:55 -0400

    addtional tests

--- tests/testthat/test-ggedf.R
+++ tests/testthat/test-ggedf.R
@@ -1,3 +1,8 @@
+test_df <- data.frame(
+  foo = c(1, 2, 2, 4, 5, 6, 1, 7),
+  bar = c(2, 3, 4, 5, 6, 6, 7, 2)
+)
+
 test_that("stat_per_sum is ggproto", {
   expect_true(ggplot2::is.ggproto(stat_per_sum()))
 })
@@ -8,4 +13,9 @@ test_that("stat_quant is ggproto", {
 
 test_that("stat_cum_per_poly is ggproto", {
   expect_true(ggplot2::is.ggproto(stat_cum_per_poly()))
+})
+
+test_that("stat_cum_per_poly has correct labels", {
+  p <- ggplot2::ggplot(test_df, ggplot2::aes(foo)) + stat_cum_per_poly()
+  expect_equal(p$labels$x, "foo")
 })
